7bb865ca294a7377486af25453002e41624b41a7
hsjoihs / jekto.vatimeliju <jekto.vatimeliju@gmail.com>
2022-08-14 17:26:19 +0900

    Translate: cp -> krantairzarth

--- apps/krantairzarth/krantairzarth.cpp
+++ apps/krantairzarth/krantairzarth.cpp
@@ -3,19 +3,19 @@
 
 extern "C" void main(int argc, char** argv) {
   if (argc < 3) {
-    printf("Usage: %s <src> <dest>\n", argv[0]);
+    printf("liusel: %s <ler> <el>\n", argv[0]);
     exit(1);
   }
 
   FILE* fp_src = fopen(argv[1], "r");
   if (fp_src == nullptr) {
-    printf("failed to open for read: %s\n", argv[1]);
+    printf("mi nix mels jielujo ny la lex fua akrantio. %s\n", argv[1]);
     exit(1);
   }
 
   FILE* fp_dest = fopen(argv[2], "w");
   if (fp_dest == nullptr) {
-    printf("failed to open for write: %s\n", argv[2]);
+    printf("mi nix mels jielujo ny la lex fua anfitiro. %s\n", argv[2]);
     exit(1);
   }
 
@@ -24,7 +24,7 @@ extern "C" void main(int argc, char** argv) {
   while ((bytes = fread(buf, 1, sizeof(buf), fp_src)) > 0) {
     const size_t written = fwrite(buf, 1, bytes, fp_dest);
     if (bytes != written) {
-      printf("failed to write to %s\n", argv[2]);
+      printf("mi nix mels anfitiro el %s\n", argv[2]);
       exit(1);
     }
   }
