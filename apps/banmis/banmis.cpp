3c8b35bfdf33cad75de4d6a89ae6eb5e3cd48dd2
hsjoihs / jekto.vatimeliju <jekto.vatimeliju@gmail.com>
2022-08-14 22:13:05 +0900

    Translate: winhello -> banmis, winjpn: tarmzi

--- apps/banmis/banmis.cpp
+++ apps/banmis/banmis.cpp
@@ -5,14 +5,14 @@
 
 extern "C" void main(int argc, char** argv) {
   auto [layer_id, err_openwin]
-    = SyscallOpenWindow(200, 100, 10, 10, "winhello");
+    = SyscallOpenWindow(200, 100, 10, 10, "banmis");
   if (err_openwin) {
     exit(err_openwin);
   }
 
-  SyscallWinWriteString(layer_id, 7, 24, 0xc00000, "hello world!");
-  SyscallWinWriteString(layer_id, 24, 40, 0x00c000, "hello world!");
-  SyscallWinWriteString(layer_id, 40, 56, 0x0000c0, "hello world!");
+  SyscallWinWriteString(layer_id, 8, 24, 0xc00000, "ban missen tonir");
+  SyscallWinWriteString(layer_id, 24, 40, 0x00c000, "l'es birleen");
+  SyscallWinWriteString(layer_id, 40, 56, 0x0000c0, "alefis io");
 
   AppEvent events[1];
   while (true) {
--- apps/tarmzi/tarmzi.cpp
+++ apps/tarmzi/tarmzi.cpp
@@ -5,14 +5,14 @@
 
 extern "C" void main(int argc, char** argv) {
   auto [layer_id, err_openwin]
-    = SyscallOpenWindow(200, 100, 10, 10, u8"こんにちは");
+    = SyscallOpenWindow(400, 100, 10, 10, u8"皇論之書");
   if (err_openwin) {
     exit(err_openwin);
   }
 
-  SyscallWinWriteString(layer_id, 7, 24, 0xc00000, u8"おはよう 世界！");
-  SyscallWinWriteString(layer_id, 24, 40, 0x00c000, u8"こんにちは 世界！");
-  SyscallWinWriteString(layer_id, 40, 56, 0x0000c0, u8"こんばんは 世界！");
+  SyscallWinWriteString(layer_id, 7, 24, 0xc00000, u8"地開混在 天光心在 心集皇来 皇生心集");
+  SyscallWinWriteString(layer_id, 24, 40, 0x00c000, u8"人心其片 心開皇行 心圧行無 唯心唯思");
+  SyscallWinWriteString(layer_id, 40, 56, 0x0000c0, u8"心圧圧来 圧来圧来 心圧悪来 心圧唯圧");
 
   AppEvent events[1];
   while (true) {
