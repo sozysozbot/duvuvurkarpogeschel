5822348a260bf867a35c1c302b938a93f61ead55
hsjoihs / jekto.vatimeliju <jekto.vatimeliju@gmail.com>
2022-08-14 23:19:37 +0900

    Remove onlyhlt; Translate fault -> iska

--- apps/iska/iska.cpp
+++ apps/iska/iska.cpp
@@ -4,20 +4,20 @@
 #include "../syscall.h"
 
 extern "C" void main(int argc, char** argv) {
-  const char* cmd = "hlt";
+  const char* cmd = "delok";
   if (argc >= 2) {
     cmd = argv[1];
   }
 
-  if (strcmp(cmd, "hlt") == 0) {
+  if (strcmp(cmd, "delok") == 0) {
     __asm__("hlt");
-  } else if (strcmp(cmd, "wr_kernel") == 0) {
+  } else if (strcmp(cmd, "kr_sysital") == 0) {
     int* p = reinterpret_cast<int*>(0x100);
     *p = 42;
-  } else if (strcmp(cmd, "wr_app") == 0) {
+  } else if (strcmp(cmd, "kr_karfaksal") == 0) {
     int* p = reinterpret_cast<int*>(0xffff8000ffff0000);
     *p = 123;
-  } else if (strcmp(cmd, "zero") == 0) {
+  } else if (strcmp(cmd, "kakites") == 0) {
     volatile int z = 0;
     printf("100/%d = %d\n", z, 100/z);
   }
